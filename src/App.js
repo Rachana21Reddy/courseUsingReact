670cfd028b10cc0f4cef65aab15dfb6b214b59a4
Rachana21Reddy <rachana21reddy@gmail.com>
2021-09-02 15:05:00 +0530

    midified code

--- src/App.js
+++ src/App.js
@@ -1,74 +1,117 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
+
+const SyllabusForm = props => {
+  const [syllabusTitle, setTitle] = useState(props.syllabusData.syllabusTitle);
+  const [description, setDescription] = useState(props.syllabusData.description);
+  const [objective, setObjective] = useState(props.syllabusData.objective);
+
+  const handleChange = event => {
+    if(event.target.name === "syllabusTitle") setTitle(event.target.value);
+    if(event.target.name === "description") setDescription(event.target.value);
+    if(event.target.name === "objective") setObjective(event.target.value);
+  }
+
+  const Syllabus={
+    "syllabusTitle":syllabusTitle,
+    "description":description,
+    "objective":objective
+  };
+
+  const handleSave = () => props.onSave(props.index,Syllabus);
+
+  const handleCancel = () => props.onCancel(props.index);
+
+  return(
+    <div id="form">
+    <br></br><strong>{props.index+1}</strong><br></br>
+    <label>Syllabus Title</label>
+    <input type="text" name="syllabusTitle" id="syllabusTitle" value={syllabusTitle} onChange={handleChange}></input>
+    <br></br>
+    <label>Description</label>
+    <input type="text" name="description" id="description" value={description} onChange={handleChange}></input>
+    <br></br>
+    <label>Objective</label>
+    <input type="text" name="objective" id="objective" value={objective} onChange={handleChange}></input>
+    <br></br>
+    <button onClick={handleSave} id="save">save</button>
+    <button onClick={handleCancel} id="cancel">cancel</button>
+    <br></br>
+    </div>
+  );
+}
+
+const SyllabusCard = props => {
+  const editSyllabus = () => props.edit(props.index);
+  const deleteSyllabus = () => props.delete(props.index);
+  return (
+    <div class="card">
+      <br></br><label class="index">{props.index+1}</label><br></br>
+      <label>syllabusTitle:{props.syllabusData.syllabusTitle}</label><br></br>
+      <label>Description:{props.syllabusData.description}</label><br></br>
+      <label>Objective:{props.syllabusData.objective}</label><br></br>
+      <button onClick={editSyllabus} id="edit">Edit</button>
+      <button onClick={deleteSyllabus} id="delete">Delete</button><br></br>
+    </div>
+  );
+}
+
 
 function App() {
-  const syllabusTitle = useRef(null);
-  const description = useRef(null);
-  const objective = useRef(null);
-
+ 
   const [syllabusItems, setSyllabusItems] =  useState([]);
-  const syllabusItemsClone = [ ...syllabusItems];
   const DisplayForm = () => {
-     syllabusItemsClone.push({
-        syllabusTitle: "",
-        description: "",
-        objective: "",
-        editMode: true
-      });
-      setSyllabusItems(syllabusItemsClone);
-  }
-
-  const SyllabusForm = (props) => {
-    return(
-      <>
-      <br></br>
-      <lable>Syllabus Title</lable>
-      <input type="text" defaultvalue={props.syllabusData.syllabusTitle} ref={syllabusTitle}></input>
-      <br></br>
-      <lable>Description</lable>
-      <input type="text" defaultvalue={props.syllabusData.description} ref={description}></input>
-      <br></br>
-      <lable>Objective</lable>
-      <input type="text" defaultvalue={props.syllabusData.objective} ref={objective}></input>
-      <br></br>
-      <button onClick={Save}>save</button>
-      <br></br>
-      </>
-    );
-  }
-  
-  const Save = () => {
-    syllabusItemsClone.pop();
+    const syllabusItemsClone = [ ...syllabusItems];
     syllabusItemsClone.push({
-      syllabusTitle: syllabusTitle.current.value,
-      description: description.current.value,
-      objective: objective.current.value,
-      editMode: false
+      syllabusTitle: "",
+      description: "",
+      objective: "",
+      editMode: true
     });
-    console.log(syllabusItemsClone);
     setSyllabusItems(syllabusItemsClone);
   }
   
-  const SyllabusCard = (props) => {
-    return (
-      <>
-        <br></br><strong>{props.number}</strong><br></br>
-        <lable>syllabusTitle:{props.syllabusData.syllabusTitle}</lable><br></br>
-        <lable>Description:{props.syllabusData.description}</lable><br></br>
-        <label>Objective:{props.syllabusData.objective}</label><br></br>
-        <button>Edit</button><br></br>
-      </>
-    );
+  const save = (index, Syllabus) => {
+    const syllabusItemsClone = [...syllabusItems];
+    console.log(Syllabus);
+    syllabusItemsClone[index] = Syllabus;
+    syllabusItemsClone[index].editMode = false;
+    console.log(syllabusItemsClone)
+    setSyllabusItems(syllabusItemsClone);
   }
 
+  const cancel = index => {
+    const syllabusItemsClone = [...syllabusItems];
+    if(syllabusItemsClone[index].syllabusTitle === "" && syllabusItemsClone[index].description === "" && syllabusItemsClone[index].objective === "") {
+      syllabusItemsClone.pop();
+    }
+    else {
+      syllabusItemsClone[index].editMode = false;
+    }
+    setSyllabusItems(syllabusItemsClone);
+  }
+
+  const handleEdit = index => {
+    const syllabusItemsClone = [...syllabusItems];
+    syllabusItemsClone[index].editMode = true;
+    setSyllabusItems(syllabusItemsClone);
+  }
+
+  const handleDelete = index => {
+    const syllabusItemsClone = [...syllabusItems];
+    syllabusItemsClone.splice(index,1);
+    setSyllabusItems(syllabusItemsClone);
+  }
+  
   return (
     <div>
-      <button onClick={DisplayForm}>Add syllabus</button>
+      <button onClick={DisplayForm} id="syllabusBtn">Add syllabus</button>
       {syllabusItems.map((syllabusItem, index) => {
-        return((syllabusItem.editMode === false ? 
-        (<SyllabusCard key={`SyllabusCard.${index}`} syllabusData={syllabusItem} number={index+1}></SyllabusCard>)
-        :(<SyllabusForm syllabusData={syllabusItem}></SyllabusForm>)))}
+        console.log(syllabusItem.editMode);
+        return((syllabusItem.editMode === true ? 
+          (<SyllabusForm key={`SyllabusForm-${index}`} syllabusData={syllabusItem} index={index} onSave={save} onCancel={cancel}></SyllabusForm>)
+          :(<SyllabusCard key={`SyllabusCard-${index}`} syllabusData={syllabusItem} index={index} edit={handleEdit} delete={handleDelete}></SyllabusCard>)))}
       )}
     </div>
     )
